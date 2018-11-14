148ca74a7bc6f1783c126c95ba41745a87086a26
zhaochengxian <zhaochengxian@soyoung.com>
2018-11-14 12:33:26 +0800

    优化

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,38 @@
+<template lang="pug">
+    div.app
+        keep-alive
+            router-view(v-if="$route.meta.keepAlive")
+        router-view(v-if="!$route.meta.keepAlive")
 
-<template>
-  <div id="app">
-    <keep-alive>
-      <router-view v-if="$route.meta.keepAlive"></router-view>
-    </keep-alive>
-    <router-view v-if="!$route.meta.keepAlive"></router-view>
-    <tabbar
-      class="tabbarStyle"
-      style="position: fixed"
-      @on-index-change="tabChange"
-      v-show="$store.state.isShowTabbar"
-    >
-      <tabbar-item link="/" selected>
-        <!--<img v-show="selectBorrow" slot="icon" src="./assets/7.png">-->
-        <!--<img v-show="!selectBorrow" slot="icon" src="./assets/4.png">-->
-        <span slot="label">首页</span>
-      </tabbar-item>
-      <tabbar-item link="/repay">
-        <!--<img v-show="selectRepay" slot="icon" src="./assets/11.png">-->
-        <!--<img v-show="!selectRepay" slot="icon" src="./assets/10.png">-->
-        <span slot="label">还款</span>
-      </tabbar-item>
-      <tabbar-item link="/memberCenter">
-        <!--<img v-show="selectMine" slot="icon" src="./assets/9.png">-->
-        <!--<img v-show="!selectMine" slot="icon" src="./assets/8.png">-->
-        <span slot="label">个人中心</span>
-      </tabbar-item>
-    </tabbar>
-  </div>
+        tabbar
+
 </template>
 
 <script>
-  import { Tabbar, TabbarItem,XHeader } from 'vux'
-  export default {
-    name: 'app',
-    data () {
-      return {
-        selectBorrow:true,
-        selectRepay:true,
-        selectMine:true
+    import Tabbar from "@/components/common/tabbar"
 
-      }
-    },
-    components: {
-      Tabbar,
-      TabbarItem,
-      XHeader,
-    },
-
-    methods:{
-      tabChange(e){
-        switch (e){
-          case 0:
-            this.selectBorrow = false
-            this.selectRepay = true
-            this.selectMine = true
-            break;
-          case 1:
-            this.selectBorrow = true
-            this.selectRepay = false
-            this.selectMine = true
-            break;
-          default:
-            this.selectBorrow = true
-            this.selectRepay = true
-            this.selectMine = false
-            break;
+    export default {
+        name: 'app',
+        components: {
+            Tabbar,
         }
-      }
     }
-  }
 </script>
 
 <style lang="less">
-  @import '~vux/src/styles/reset.less';
-  @media screen and (min-width: 520px) {
-    #app{
-      font-family:DIN;
-      width: 375px;
-      height: 667px;
-      display: flex;
-      margin: 0 auto;
+    @import '~vux/src/styles/reset.less';
+
+    @media screen and (min-width: 520px) {
+        .app {
+            .w(375px);
+            .h(667px)
+            .flex();
+            .m2(0, auto)
+            .tabbarStyle {
+                .w(100%)
+                .m2(0, auto)
+            }
+        }
     }
-    #app .tabbarStyle{
-      width: 100%;
-      margin: 0 auto;
-    }
-  }
 
 </style>
---
+++ src/components/common/tabbar.vue
@@ -0,0 +1,70 @@
+<template>
+    <div>
+        <tabbar
+                class="tabbarStyle"
+                style="position: fixed"
+                @on-index-change="tabChange"
+                v-show="$store.state.isShowTabbar"
+        >
+            <tabbar-item link="/" selected>
+                <!--<img v-show="selectBorrow" slot="icon" src="./assets/7.png">-->
+                <!--<img v-show="!selectBorrow" slot="icon" src="./assets/4.png">-->
+                <span slot="label">首页</span>
+            </tabbar-item>
+            <tabbar-item link="/repay">
+                <!--<img v-show="selectRepay" slot="icon" src="./assets/11.png">-->
+                <!--<img v-show="!selectRepay" slot="icon" src="./assets/10.png">-->
+                <span slot="label">还款</span>
+            </tabbar-item>
+            <tabbar-item link="/memberCenter">
+                <!--<img v-show="selectMine" slot="icon" src="./assets/9.png">-->
+                <!--<img v-show="!selectMine" slot="icon" src="./assets/8.png">-->
+                <span slot="label">个人中心</span>
+            </tabbar-item>
+        </tabbar>
+    </div>
+</template>
+
+<script>
+    import { Tabbar, TabbarItem} from 'vux'
+    export default {
+        name: "tabbar",
+        data () {
+            return {
+                selectBorrow:true,
+                selectRepay:true,
+                selectMine:true
+
+            }
+        },
+        components: {
+            Tabbar,
+            TabbarItem
+        },
+        methods:{
+            tabChange(e){
+                switch (e){
+                    case 0:
+                        this.selectBorrow = false
+                        this.selectRepay = true
+                        this.selectMine = true
+                        break;
+                    case 1:
+                        this.selectBorrow = true
+                        this.selectRepay = false
+                        this.selectMine = true
+                        break;
+                    default:
+                        this.selectBorrow = true
+                        this.selectRepay = true
+                        this.selectMine = false
+                        break;
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
